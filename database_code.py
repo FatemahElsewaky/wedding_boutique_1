baf479d7d31a857afd714c807ba3c65022225544
oshunose <115500904+oshunose@users.noreply.github.com>
2024-04-22 19:36:58 -0400

    Update database_code.py

--- database_code.py
+++ database_code.py
@@ -5,14 +5,12 @@ c = conn.cursor()
 
 # Create user table
 c.execute(
-    """CREATE TABLE IF NOT EXISTS user (
+    """CREATE TABLE IF NOT EXISTS users (
           username VARCHAR(20) PRIMARY KEY,
           password VARCHAR(15),
           first_name VARCHAR(20),
           last_name VARCHAR(20),
           address_1 VARCHAR(50),
-          address_2 VARCHAR(50),
-          payment_info VARCHAR(20),
           email VARCHAR(50),
           phone_number INTEGER,
           order_history TEXT
@@ -22,17 +20,18 @@ c.execute(
 # Create payment information table
 c.execute(
     """CREATE TABLE IF NOT EXISTS payment_info (
-          username VARCHAR(20) PRIMARY KEY,
-          credit_card_num INT(16),
+          payment_id VARCHAR(20) PRIMARY KEY,
+          credit_card_num INTEGER(16),
           CVC INT(3),
-          expiration_date VARCHAR(5)
+          expiration_date VARCHAR(5),
+          FOREIGN KEY (payment_id) REFERENCES user(username) ON DELETE CASCADE
           )"""
 )
 
 # Create employee table
 c.execute(
-    """CREATE TABLE IF NOT EXISTS user (
-          employee_id VARCHAR(20) PRIMARY KEY,
+    """CREATE TABLE IF NOT EXISTS employees (
+          employee_id INTEGER(9) PRIMARY KEY,
           username VARCHAR(20),
           password VARCHAR(15),
           first_name VARCHAR(20),
